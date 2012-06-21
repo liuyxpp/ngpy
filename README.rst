80e927306f38a7f2a440b155f8fe1725f60348a2
Yi-Xin Liu <liuyxpp@gmail.com>
2012-06-21 17:15:52 +0800

    Update README. Ready for releasing version 0.3.

--- README.rst
+++ README.rst
@@ -1,7 +1,10 @@
 NGPy
 ~~~~
 
-NGPy is a web application that enable online performing and analyzing Monte-Carlo simulation on nucleation and growth phenomena. It can be also used as a web framework to develop your own web applications. NGPy is built on top of Flask.
+NGPy is a web application that enable online performing and analyzing Monte-Carlo simulations on nucleation and growth phenomena. 
+It can be also used as a web framework to develop your own web applications.
+NGPy is built on top of **Flask**.
+NGPy is actually a distributed system since we use **ZEO server** as a database and **redis** server as a message queue.
 
 Quickstart
 ``````````
@@ -21,18 +24,21 @@ or
     $ cd ngpy-xxx
     $ python setup.py install
 
-* Make sure that the NoSQL database redis is installed in your system.
-* Other dependencies like ZODB, WTForm, Numpy, and Matplotlib should be
-  taken care by easy_install. If not, try to install them mannually.
+* Make sure that the NoSQL database **redis** is installed in your system.
 
-2. Start ZODB server
+* Other dependencies such as **ZODB3**, **WTForm**, **Numpy**, and **Matplotlib** should be taken care by *easy_install*. 
+  If not, try to install them mannually.
+
+2. Start ZEO server
 --------------------
 
 ::
 
-    $ runzeo -a 0.0.0.0:<ZEOPORT> -f /path/to/your/data.fs
+    $ runzeo -a 0.0.0.0:<ZEOPORT> -f /path/to/your/Data.fs
 
 * <ZEOPORT> must be an available port number, e.g. 1234
+
+* The **ZEO** server should run at a host that other hosts can connect to. 
 
 3. Start redis server
 ---------------------
@@ -41,6 +47,8 @@ or
 
     $ redis-server [/path/to/redis.conf]
 
+* The **redis** server should run at a host that other hosts can connect to.
+
 4. Start the task queue daemon
 ------------------------------
 
@@ -48,14 +56,23 @@ or
 
     $ simd [-r <server>] [-q <qkey>]
 
+* <server> is the host where you run the **redis** server.
+* What **simd** does is that it just picks the messages from **redis** server and process them.
+  So you can run **simd** at every hosts where you wish to run the task.
+
 5. Start ngpy
 -------------
 
 ::
 
-    $ run-ngpy
+    $ run-ngpy [-c </path/to/ngpy.cfg>]
 
-The ngpy website should be now served at http://localhost:5000.
+The *ngpy.cfg* is the configuration file for **ngpy**. 
+It is a Python file and will be processed by *Flask*.
+Only values in uppercase are actually used.
+More details are refered to the **Flask** documentation.
+
+The **ngpy** website should be now served at http://localhost:5000.
 You can visit it use any browser (Chrome, Firefox, IE, etc.) via
 http://localhost:5000
 if you are a local visitor, or
@@ -66,11 +83,14 @@ Ask for Help
 ````````````
 
 * You can directly contact me at liuyxpp@gmail.com.
-* You can join the mailinglist by sending an email to ngpy@librelist.com and replying to the confirmation mail. To unsubscribe, send a mail to ngpy-unsubscribe@librelist.com and reply to the confirmation mail.
+
+* You can join the mailinglist by sending an email to ngpy@librelist.com and replying to the confirmation mail. 
+  To unsubscribe, send a mail to ngpy-unsubscribe@librelist.com and reply to the confirmation mail.
 
 Links
 `````
 
 * `Website <http://liuyxpp.bitbucket.org>`_
+
 * `Development version <http://bitbucket.org/liuyxpp/ngpy/>`_
 
--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 NGPy
 ~~~~
 
-NGPy is a web application that enable online performing and analyzing Monte-Carlo simulation on nucleation and growth phenomena. It can be also used as a web framework to develop your own web applications. NGPy is built on top of Flask.
+NGPy is a web application that enable online performing and analyzing Monte-Carlo simulations on nucleation and growth phenomena. 
+It can be also used as a web framework to develop your own web applications.
+NGPy is built on top of **Flask**.
+NGPy is actually a distributed system since we use **ZEO server** as a database and **redis** server as a message queue.
 
 Quickstart
 ``````````
@@ -22,18 +25,21 @@ or
     $ cd ngpy-xxx
     $ python setup.py install
 
-* Make sure that the NoSQL database redis is installed in your system.
-* Other dependencies like ZODB, WTForm, Numpy, and Matplotlib should be
-  taken care by easy_install. If not, try to install them mannually.
+* Make sure that the NoSQL database **redis** is installed in your system.
 
-2. Start ZODB server
+* Other dependencies such as **ZODB3**, **WTForm**, **Numpy**, and **Matplotlib** should be taken care by *easy_install*. 
+  If not, try to install them mannually.
+
+2. Start ZEO server
 --------------------
 
 ::
 
-    $ runzeo -a 0.0.0.0:<ZEOPORT> -f /path/to/your/data.fs
+    $ runzeo -a 0.0.0.0:<ZEOPORT> -f /path/to/your/Data.fs
 
 * <ZEOPORT> must be an available port number, e.g. 1234
+
+* The **ZEO** server should run at a host that other hosts can connect to. 
 
 3. Start redis server
 ---------------------
@@ -42,33 +48,52 @@ or
 
     $ redis-server [/path/to/redis.conf]
 
+* The **redis** server should run at a host that other hosts can connect to.
+
 4. Start the task queue daemon
 ------------------------------
 
 ::
 
-    $ simd
+    $ simd [-r <server>] [-q <qkey>]
+
+* <server> is the host where you run the **redis** server.
+* What **simd** does is that it just picks the messages from **redis** server and process them.
+  So you can run **simd** at every hosts where you wish to run the task.
 
 5. Start ngpy
 -------------
 
 ::
 
-    $ run-ngpy
+    $ run-ngpy [-c </path/to/ngpy.cfg>]
 
-The ngpy website should be now served at http://localhost:5000.
+The *ngpy.cfg* is the configuration file for **ngpy**. 
+It is a Python file and will be processed by *Flask*.
+Only values in uppercase are actually used.
+More details are refered to the **Flask** documentation.
+
+The **ngpy** website should be now served at http://localhost:5000.
 You can visit it use any browser (Chrome, Firefox, IE, etc.) via
 http://localhost:5000
 if you are a local visitor, or
-http://IP.of.NGPy.run:5000
+http://IP.of.NGPy.run:5000 
 if you visit remotely.
+
+Ask for Help
+````````````
+
+* You can directly contact me at liuyxpp@gmail.com.
+
+* You can join the mailinglist by sending an email to ngpy@librelist.com and replying to the confirmation mail. 
+  To unsubscribe, send a mail to ngpy-unsubscribe@librelist.com and reply to the confirmation mail.
 
 Links
 `````
 
 * `Website <http://liuyxpp.bitbucket.org>`_
-* `Development version <http://bitbucket.org/liuyxpp/ngpy/>`_
 
+* `Development version <http://bitbucket.org/liuyxpp/ngpy/>`_
 '''
 try:
     from setuptools import setup
@@ -77,14 +102,14 @@ except ImportError:
 
 setup(
     name='ngpy',
-    version='0.1',
+    version='0.3',
     license='BSD',
     description='Web application for Monte Carlo simulation on nucleation and growth phenomena',
     author='Yi-Xin Liu',
     author_email='liuyxpp@gmail.com',
     url='https://bitbucket.org/liuyxpp/ngpy',
     packages=['ngpy'],
-    scripts=['run-ngpy','simd'],
+    scripts=['run-ngpy','simd','manage-ngpy'],
     include_package_data=True,
     zip_safe=False,
     long_description=__doc__,
@@ -98,7 +123,7 @@ setup(
         'redis>=2.4.9',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
